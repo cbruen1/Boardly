03b71f8bda90208bd92bf0e558c229c493571e4c
Ciaran Bruen <cbruen1@yahoo.com>
2015-07-07 01:41:01 +0200

    Unit tests
    Added new unit test project with basic C# and Jasmine tests

--- MessageBoard/js/home-index.js
+++ MessageBoard/js/home-index.js
@@ -1,6 +1,6 @@
-﻿var module = angular.module("homeIndex", ['ngRoute']);
+﻿var homeIndexModule = angular.module("homeIndex", ['ngRoute']);
 
-module.config(["$routeProvider", function ($routeProvider) {
+homeIndexModule.config(["$routeProvider", function ($routeProvider) {
     $routeProvider
         .when("/", {
             controller: "topicsController",
@@ -18,7 +18,7 @@ module.config(["$routeProvider", function ($routeProvider) {
     $routeProvider.otherwise({ redirectTo: "/" });
 }]);
 
-module.factory("dataService", ["$http", "$q", function ($http, $q) {
+homeIndexModule.factory("dataService", ["$http", "$q", function ($http, $q) {
     var _topics = [];
     var _isInit = false;
 
@@ -138,7 +138,7 @@ module.factory("dataService", ["$http", "$q", function ($http, $q) {
     };
 }]);
  
-module.controller('topicsController', ["$scope", "$http", "dataService", function ($scope, $http, dataService) {
+homeIndexModule.controller('topicsController', ["$scope", "$http", "dataService", function ($scope, $http, dataService) {
     $scope.data = dataService;
     $scope.isBusy = false;
 
@@ -159,7 +159,7 @@ module.controller('topicsController', ["$scope", "$http", "dataService", functio
     };
 }]);
 
-module.controller('newTopicController', ["$scope", "$http", "$window", "dataService", function ($scope, $http, $window, dataService) {
+homeIndexModule.controller('newTopicController', ["$scope", "$http", "$window", "dataService", function ($scope, $http, $window, dataService) {
     $scope.newTopic = {};
 
     $scope.save = function () {
@@ -175,7 +175,7 @@ module.controller('newTopicController', ["$scope", "$http", "$window", "dataServ
     };
 }]);
 
-module.controller('singleTopicController', ["$scope", "dataService", "$window", "$routeParams", function ($scope, dataService, $window, $routeParams) {
+homeIndexModule.controller('singleTopicController', ["$scope", "dataService", "$window", "$routeParams", function ($scope, dataService, $window, $routeParams) {
     $scope.topic = null;
     $scope.newReply = {};
 
